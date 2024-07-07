1a754c4522a4fc3093cdbfe77403b30bd848eb86
youngboypimpin <rjtekdom@gmail.com>
2024-07-07 11:35:46 +0100

    finished

---
+++ stage-1/my-personal-website/index.js
@@ -0,0 +1,23 @@
+alert("my project site, have fun!");
+
+const dayOfWeek = [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+];
+
+const d = new Date();
+let day = dayOfWeek[d.getDay()];
+document.getElementById("day").innerHTML = day;
+
+function displayUTCTime(){
+    const now = new Date();
+    const hours = String(now.getUTCHours()).padStart(2, '0');
+    const minutes = String(now.getUTCMinutes()).padStart(2, '0');
+    const seconds = String(now.getUTCSeconds()).padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds} UTC`;
+    document.getElementById("time").innerText = timeString;
+
+}
+
+setInterval(displayUTCTime, 1000);
+
+displayUTCTime();
